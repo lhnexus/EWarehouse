4ddf12331879a3674400b30cc81c9a26a8ed3c40
Hong Liu <hong.liu02@sap.com>
2017-06-29 04:47:50 +0000

    update map

--- EWarehouseUI/resources/Panels/Charts.controller.js
+++ EWarehouseUI/resources/Panels/Charts.controller.js
@@ -1,8 +1,22 @@
-sap.ui.controller("Panels.Charts", {
-	onInit: function() {
+sap.ui.define(['sap/m/MessageToast'],
+	function (MessageToast){
+	"use strict";
+ 
+ 	return sap.ui.controller("Panels.Charts", {
+		onInit: function() {
 			
-	}
+	
+		},
+		
+		onAfterRendering: function(){
+		
+		},
+		
+		
+		press: function (oEvent) {
+			MessageToast.show("The column micro chart is pressed.");
+		}
+		
+		
+ 	});
 });
-
-
-
--- EWarehouseUI/resources/Panels/GeoMap/factory.controller.js
+++ EWarehouseUI/resources/Panels/GeoMap/factory.controller.js
@@ -21,25 +21,36 @@ sap.ui.define(['sap/m/MessageToast'],
 						"maxLOD": "18",
 						"Source": [{
 							"id": "s1",
-							// "url": "http://a.tile.openstreetmap.org/{LOD}/{X}/{Y}.png"
+							//"url": "http://1.base.maps.cit.api.here.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/256/png8?app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg"
+							//"url": "https://online1.map.bdimg.com/onlinelabel/?qt=tile&x={X}&y={Y}&z={LOD}"
+							//"url": "https://p1.map.gtimg.com/maptilesv2/{LOD}/{X/16}/{Y/16}/{X}_{Y}.png"
+							 "url": "https://a.tile.openstreetmap.org/{LOD}/{X}/{Y}.png"
 							//"url": "http://a.tile.openstreetmap.fr/hot/{LOD}/{X}/{Y}.png"
 							//"url": "http://a.tile.stamen.com/toner/{LOD}/{X}/{Y}.png"
-							"url": "http://a.tile2.opencyclemap.org/transport/{LOD}/{X}/{Y}.png"
+							//"url": "http://a.tile2.opencyclemap.org/transport/{LOD}/{X}/{Y}.png"
 								//"url": "http://a.basemaps.cartocdn.com/light_all/{LOD}/{X}/{Y}.png"
 
-						}, {
+					   }, {
 							"id": "s2",
-							//"url": "http://b.tile.openstreetmap.org/{LOD}/{X}/{Y}.png"
+							//"url": "http://2.base.maps.cit.api.here.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/256/png8?app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg"
+							//"url": "https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
+							//"url": "https://online2.map.bdimg.com/onlinelabel/?qt=tile&x={X}&y={Y}&z={LOD}"
+							//"url": "https://p2.map.gtimg.com/maptilesv2/{LOD}/{X/16}/{Y/16}/{X}_{Y}.png"
+							"url": "https://b.tile.openstreetmap.org/{LOD}/{X}/{Y}.png"
 							//"url": "http://b.tile.openstreetmap.fr/hot/{LOD}/{X}/{Y}.png"
 							//"url": "http://b.tile.stamen.com/toner/{LOD}/{X}/{Y}.png"
-							"url": "http://b.tile2.opencyclemap.org/transport/{LOD}/{X}/{Y}.png"
+							//"url": "http://b.tile2.opencyclemap.org/transport/{LOD}/{X}/{Y}.png"
 								//"url": "http://b.basemaps.cartocdn.com/light_all/{LOD}/{X}/{Y}.png"
 						}, {
 							"id": "s3",
-							//"url": "http://c.tile.openstreetmap.org/{LOD}/{X}/{Y}.png"
+							//"url": "http://3.base.maps.cit.api.here.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/256/png8?app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg"
+							//"url": "https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
+							//"url": "https://online3.map.bdimg.com/onlinelabel/?qt=tile&x={X}&y={Y}&z={LOD}"
+							//"url": "https://p3.map.gtimg.com/maptilesv2/{LOD}/{X/16}/{Y/16}/{X}_{Y}.png"
+							"url": "https://c.tile.openstreetmap.org/{LOD}/{X}/{Y}.png"
 							//"url": "http://c.tile.openstreetmap.fr/hot/{LOD}/{X}/{Y}.png"
 							//"url": "http://c.tile.stamen.com/toner/{LOD}/{X}/{Y}.png"
-							"url": "http://c.tile2.opencyclemap.org/transport/{LOD}/{X}/{Y}.png"
+							//"url": "http://c.tile2.opencyclemap.org/transport/{LOD}/{X}/{Y}.png"
 								//"url": "http://c.basemaps.cartocdn.com/light_all/{LOD}/{X}/{Y}.png"
 						}]
 					}],
@@ -103,7 +114,7 @@ sap.ui.define(['sap/m/MessageToast'],
 			onAfterRendering: function() {
 				var mPage = this.getView().byId("factpage"); //Get Hold of page
 				mPage.scrollTo(0,0);
-				MessageToast.show("After rendering.");
+				//MessageToast.show("After rendering.");
 			},
 
 			onClickAreas: function() {
