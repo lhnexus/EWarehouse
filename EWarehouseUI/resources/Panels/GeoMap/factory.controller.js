53e665eea79b543d5d050e53df241b3f47ef852f
i323921 <hong.liu02@sap.com>
2017-06-29 17:25:28 +0800

    update map factory

--- EWarehouseUI/resources/Panels/GeoMap/factory.controller.js
+++ EWarehouseUI/resources/Panels/GeoMap/factory.controller.js
@@ -24,22 +24,22 @@ sap.ui.define(['sap/m/MessageToast'],
 							//"url": "http://1.base.maps.cit.api.here.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/256/png8?app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg"
 							//"url": "https://online1.map.bdimg.com/onlinelabel/?qt=tile&x={X}&y={Y}&z={LOD}"
 							//"url": "https://p1.map.gtimg.com/maptilesv2/{LOD}/{X/16}/{Y/16}/{X}_{Y}.png"
-							 "url": "https://a.tile.openstreetmap.org/{LOD}/{X}/{Y}.png"
-							//"url": "http://a.tile.openstreetmap.fr/hot/{LOD}/{X}/{Y}.png"
-							//"url": "http://a.tile.stamen.com/toner/{LOD}/{X}/{Y}.png"
-							//"url": "http://a.tile2.opencyclemap.org/transport/{LOD}/{X}/{Y}.png"
+							"url": "https://a.tile.openstreetmap.org/{LOD}/{X}/{Y}.png"
+								//"url": "http://a.tile.openstreetmap.fr/hot/{LOD}/{X}/{Y}.png"
+								//"url": "http://a.tile.stamen.com/toner/{LOD}/{X}/{Y}.png"
+								//"url": "http://a.tile2.opencyclemap.org/transport/{LOD}/{X}/{Y}.png"
 								//"url": "http://a.basemaps.cartocdn.com/light_all/{LOD}/{X}/{Y}.png"
 
-					   }, {
+						}, {
 							"id": "s2",
 							//"url": "http://2.base.maps.cit.api.here.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/256/png8?app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg"
 							//"url": "https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
 							//"url": "https://online2.map.bdimg.com/onlinelabel/?qt=tile&x={X}&y={Y}&z={LOD}"
 							//"url": "https://p2.map.gtimg.com/maptilesv2/{LOD}/{X/16}/{Y/16}/{X}_{Y}.png"
 							"url": "https://b.tile.openstreetmap.org/{LOD}/{X}/{Y}.png"
-							//"url": "http://b.tile.openstreetmap.fr/hot/{LOD}/{X}/{Y}.png"
-							//"url": "http://b.tile.stamen.com/toner/{LOD}/{X}/{Y}.png"
-							//"url": "http://b.tile2.opencyclemap.org/transport/{LOD}/{X}/{Y}.png"
+								//"url": "http://b.tile.openstreetmap.fr/hot/{LOD}/{X}/{Y}.png"
+								//"url": "http://b.tile.stamen.com/toner/{LOD}/{X}/{Y}.png"
+								//"url": "http://b.tile2.opencyclemap.org/transport/{LOD}/{X}/{Y}.png"
 								//"url": "http://b.basemaps.cartocdn.com/light_all/{LOD}/{X}/{Y}.png"
 						}, {
 							"id": "s3",
@@ -48,9 +48,9 @@ sap.ui.define(['sap/m/MessageToast'],
 							//"url": "https://online3.map.bdimg.com/onlinelabel/?qt=tile&x={X}&y={Y}&z={LOD}"
 							//"url": "https://p3.map.gtimg.com/maptilesv2/{LOD}/{X/16}/{Y/16}/{X}_{Y}.png"
 							"url": "https://c.tile.openstreetmap.org/{LOD}/{X}/{Y}.png"
-							//"url": "http://c.tile.openstreetmap.fr/hot/{LOD}/{X}/{Y}.png"
-							//"url": "http://c.tile.stamen.com/toner/{LOD}/{X}/{Y}.png"
-							//"url": "http://c.tile2.opencyclemap.org/transport/{LOD}/{X}/{Y}.png"
+								//"url": "http://c.tile.openstreetmap.fr/hot/{LOD}/{X}/{Y}.png"
+								//"url": "http://c.tile.stamen.com/toner/{LOD}/{X}/{Y}.png"
+								//"url": "http://c.tile2.opencyclemap.org/transport/{LOD}/{X}/{Y}.png"
 								//"url": "http://c.basemaps.cartocdn.com/light_all/{LOD}/{X}/{Y}.png"
 						}]
 					}],
@@ -86,14 +86,16 @@ sap.ui.define(['sap/m/MessageToast'],
 
 				//this.oGeoMap.setInitialPosition(lon + ";" + lat + ";0");
 				this.oGeoMap.setInitialPosition("114.04109;22.65375;0");
-				this.oGeoMap.setVisualFrame({
-					"minLon": 114.03829,
-					"maxLon": 114.04337,
-					"minLat": 22.65158,
-					"maxLat": 22.65596,
-					"minLOD": 18
-				});
+				// this.oGeoMap.setVisualFrame({
+				// 	"minLon": 114.03829,
+				// 	"maxLon": 114.04337,
+				// 	"minLat": 22.65158,
+				// 	"maxLat": 22.65596,
+				// 	"minLOD": 18
+				// });
+				this.oGeoMap.setDisablePan(true);
 				
+
 				this.oPage = this.getView().byId("factpage"); //Get Hold of page
 
 				////////////////////HBOX Codes
@@ -113,7 +115,7 @@ sap.ui.define(['sap/m/MessageToast'],
 
 			onAfterRendering: function() {
 				var mPage = this.getView().byId("factpage"); //Get Hold of page
-				mPage.scrollTo(0,0);
+				mPage.scrollTo(0, 0);
 				//MessageToast.show("After rendering.");
 			},
 
@@ -123,17 +125,32 @@ sap.ui.define(['sap/m/MessageToast'],
 				// this.getView().byId("fixroot").scrollTo(0,0);
 				MessageToast.show("The factory is pressed.");
 			},
-			
-			onClickFA  : function() {
-				this.getView().byId("GeoMap").getModel().refresh(true);
-				MessageToast.show("FA is pressed.");
+
+			onClickFA: function() {
+				this.oGeoMap.setInitialPosition("114.04109;22.65375;0");
+				this.oGeoMap.setInitialZoom(18);
+				// sap.ui.getCore().byId("factpage").getModel().refresh(true);
+				// MessageToast.show("FA is pressed.");
+			},
+			onClickFB: function() {
+
+				this.oGeoMap.setInitialPosition("113.8457;34.5533;0");
+				this.oGeoMap.setInitialZoom(15);
+				// MessageToast.show("FA is pressed.");
 			},
 
-			onClickmap: function() {
+			onClickFC: function() {
 
-				this.oPage.scrollTo(0, 0);
-				// this.getView().byId("fixroot").scrollTo(0,0);
-				MessageToast.show("The GeoMap is pressed.");
+				this.oGeoMap.setInitialPosition("114.1073;23.1415;0");
+				this.oGeoMap.setInitialZoom(16);
+				//MessageToast.show("The GeoMap is pressed.");
+			},
+
+			onClickFD: function() {
+
+				this.oGeoMap.setInitialPosition("112.5844;37.7467;0");
+				this.oGeoMap.setInitialZoom(16);
+				//MessageToast.show("The GeoMap is pressed.");
 			},
 			// onMarkerClick: function(oEvent) {
 			// 	this.oMap.setOptions({styles: this.ostyles["silver"]});
