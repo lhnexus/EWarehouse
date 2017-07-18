3ac13dd5b0ba53440eda159e1c6f7fd1911cbfaa
lhnexus <liuhongdashi@hotmail.com>
2017-07-18 22:30:50 +0800

    improve tracking page

--- EWarehouseUI/resources/Canvas/Track/robotDetail.controller.js
+++ EWarehouseUI/resources/Canvas/Track/robotDetail.controller.js
@@ -1,6 +1,22 @@
 sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/model/odata/v2/ODataModel", "../Util/svgUtil","sap/m/MessageToast"],
     function (Controller, ODataModel, MessageToast) {
         "use strict";
+        function convertData(oEvent) {
+            var oData,
+                oModel = oEvent.getSource();
+                // sContextPath = getContextPath();
+
+            if (!oEvent.getParameters().success) {
+                return;
+            }
+
+            oData = oModel.getData();
+            oData.GoodTimeLine.forEach(function (oGTimeLine) {
+                oGTimeLine.time = DateUtils.parseDate(oGTimeLine.time);
+
+            });
+            oModel.updateBindings(true);
+        }
 
         return Controller.extend("Canvas.Track.robotDetail", {
             onInit: function () {
@@ -10,6 +26,9 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/model/odata/v2/ODataModel",
                 var localModel = new sap.ui.model.json.JSONModel();
 
                 localModel.setData( oModel.getData().Cars[0]);
+
+                localModel.attachRequestCompleted(convertData);
+
                 this.getView().setModel(localModel);
 
                 this.getView().byId("TimePanel").addStyleClass("sapUiPanelCont");
