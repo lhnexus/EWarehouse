c413a7874b8643a9fcdca7858926df0777e0cd28
Hong Liu <hong.liu02@sap.com>
2017-08-02 06:46:08 +0000

    iot enhance

--- EWarehouseUI/resources/Canvas/Track/humidity.controller.js
+++ EWarehouseUI/resources/Canvas/Track/humidity.controller.js
@@ -1,39 +1,63 @@
 sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "../Util/svgUtil"],
-	function (Controller,MessageToast){
-	"use strict";
- 
- 	return Controller.extend("Canvas.Track.humidity", {
-		onInit: function() {
+	function(Controller, MessageToast) {
+		"use strict";
 
-            var oModel = new sap.ui.model.json.JSONModel();
-            oModel.loadData("Canvas/mockserver/data.json");
-            oModel.attachRequestCompleted(calRack);
-            this.getView().setModel(oModel);
+		return Controller.extend("Canvas.Track.humidity", {
+			onInit: function() {
 
+				var oModel = new sap.ui.model.json.JSONModel();
+				var oRModel = new sap.ui.model.odata.v2.ODataModel("/iotmms/v1/api/http/app.svc");
+				oModel.loadData("Canvas/mockserver/data.json");
+				oModel.attachRequestCompleted(calRack);
+				this.getView().setModel(oModel, "jdata");
+				this.getView().setModel(oRModel, "odata");
 
-		},
+			},
 
-        onAfterRendering: function(){
+			onAfterRendering: function() {
 
-			var that = this;
-            that._loadData(that);
+				var that = this;
+				that._loadData(that);
 
-        },
+			},
 
+			_loadData: function(that) {
 
-        _loadData: function(that){
+				setInterval(function() {
+					
+					var filter = new Array();
+					var sorter = new Array();
+					var sorting = new sap.ui.model.Sorter("C_TIMESTAMP", true);
+					var filtering = new sap.ui.model.Filter("C_DEVICEID", sap.ui.model.FilterOperator.EQ, "RUFF02");
+					sorter.push(sorting);
+					filter.push(filtering);
 
+					that.getView().getModel("odata").read("/T_IOT_079BCC4BE4FD6487DE4B", {
+						filters: filter,
+						sorters: sorter,
+						async: false,
+						success: function(oData, response) {
+							//var len = oData.results.length;
+							//var time = oData.results[0].C_TIMESTAMP;
+							var humidity = oData.results[0].C_HUMIDITY;
+							var hnum = that.getView().byId("humiditynum");
+							hnum.setValue(parseFloat(humidity));
+							//console.log(time);
+							console.log(parseFloat(humidity));
+						},
+						failed: function(oData, response) {
+								alert("Failed to get InputHelpValues from service!");
+							}
+							//console.log(oMetadata);
+					}, null);
 
-            setInterval(function(){
+					// var num = Math.floor(Math.random()*10);
+					// var hnum = that.getView().byId("humiditynum");
+					// hnum.setValue(num);
 
-                var num = Math.floor(Math.random()*10);
-                var hnum = that.getView().byId("humiditynum");
-                hnum.setValue(num);
+				}, 1000);
 
+			}
 
-            },1000);
-
-        }
-		
- 	});
-});
+		});
+	});
--- EWarehouseUI/resources/Canvas/Track/temperature.controller.js
+++ EWarehouseUI/resources/Canvas/Track/temperature.controller.js
@@ -1,48 +1,70 @@
 sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "../Util/svgUtil"],
-	function (Controller,MessageToast){
-	"use strict";
- 
- 	return Controller.extend("Canvas.Track.temperature", {
-		onInit: function() {
+	function(Controller, MessageToast) {
+		"use strict";
 
-            var oModel = new sap.ui.model.json.JSONModel();
-            oModel.loadData("Canvas/mockserver/data.json");
+		return Controller.extend("Canvas.Track.temperature", {
+			onInit: function() {
 
-            var oModel = new sap.ui.model.odata.v2.ODataModel("https://iotmmsi323921trial.hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/http/app.svc");
+				var oModel = new sap.ui.model.json.JSONModel();
+				var oRModel = new sap.ui.model.odata.v2.ODataModel("/iotmms/v1/api/http/app.svc");
 
-            oModel.attachMetadataLoaded(null, function(){
-                var oMetadata = oModel.getServiceMetadata();
-                console.log(oMetadata);
-            },null);
+				oModel.loadData("Canvas/mockserver/data.json");
 
+				oModel.attachRequestCompleted(calRack);
+				this.getView().setModel(oModel, "jdata");
+				this.getView().setModel(oRModel, "odata");
 
-            oModel.attachRequestCompleted(calRack);
-            this.getView().setModel(oModel);
+			},
 
+			onAfterRendering: function() {
 
-		},
+				var that = this;
+				that._loadData(that);
 
-        onAfterRendering: function(){
+			},
 
-            var that = this;
-            that._loadData(that);
+			_loadData: function(that) {
 
-        },
+				setInterval(function() {
 
+					// oRModel.attachMetadataLoaded(null, function() {
+					// 	var oMetadata = oRModel.getServiceMetadata();
+					// 	console.log(oMetadata);
+					// }, null);
 
-        _loadData: function(that){
+					var filter = new Array();
+					var sorter = new Array();
+					var sorting = new sap.ui.model.Sorter("C_TIMESTAMP", true);
+					var filtering = new sap.ui.model.Filter("C_DEVICEID", sap.ui.model.FilterOperator.EQ, "RUFF02");
+					sorter.push(sorting);
+					filter.push(filtering);
 
+					that.getView().getModel("odata").read("/T_IOT_079BCC4BE4FD6487DE4B", {
+						filters: filter,
+						sorters: sorter,
+						async: false,
+						success: function(oData, response) {
+							//var len = oData.results.length;
+							//var time = oData.results[0].C_TIMESTAMP;
+							var temperature = oData.results[0].C_TEMPERATURE;
+							var hnum = that.getView().byId("temperaturenum");
+							hnum.setValue(parseFloat(temperature));
+							//console.log(time);
+							console.log(parseFloat(temperature));
+						},
+						failed: function(oData, response) {
+								alert("Failed to get InputHelpValues from service!");
+							}
+							//console.log(oMetadata);
+					}, null);
 
-            setInterval(function(){
+					//var num = Math.floor(Math.random() * 10);
+					//var hnum = that.getView().byId("temperaturenum");
+					//hnum.setValue(num);
 
-                var num = Math.floor(Math.random()*10);
-                var hnum = that.getView().byId("temperaturenum");
-                hnum.setValue(num);
+				}, 2000);
 
+			}
 
-            },1000);
-
-        }
-		
- 	});
-});
+		});
+	});
